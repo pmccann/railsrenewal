0699e04b7cbe02efe12f06a21a34220e0a950386
Paul McCann <pmccann@gmail.com>
2018-08-31 23:05:26 +0200

    Added view for edit action in article controller

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,6 +3,10 @@ class ArticlesController < ApplicationController
     def new
         @article = Article.new
     end
+    
+    def edit
+        @article = Article.find(params[:id])
+    end
 
     def create
          @article=Article.new(article_params)
