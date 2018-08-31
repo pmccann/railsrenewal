3831943155b4a688162e66d80eeffb501e2f6033
Paul McCann <pmccann@gmail.com>
2018-08-31 15:10:22 +0200

    Adding some error messages to view, and tidying up articles controller

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # railsrenewal
 Coming back to Rails 4 after about 5 years
 
+OK, let's see if this is picked up in "Working Copy"
+
 And here's the way forward. `vielleicht` 
 
 Checking for push behaviour. Sure, and while we're at it, let's check on the behaviour via Textastic. 
--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,13 +1,18 @@
 class ArticlesController < ApplicationController
 
     def new
+        @article = Article.new
     end
 
     def create
-         @article=Article.new(params.require(:article).permit(:title,:text))
+         @article=Article.new(article_params)
 
-         @article.save
-         redirect_to @article
+         if @article.save
+              redirect_to @article
+         else
+              render 'new'
+         end
+
     end
 
     def show
@@ -17,5 +22,10 @@ class ArticlesController < ApplicationController
     def index
         @articles=Article.all
     end
+
+private
+  def article_params
+    params.require(:article).permit(:title, :text)
+  end
 
 end
